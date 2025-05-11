c6f251478c524a5a6810463ae8592e4e2e77362e
Alexander Baker <Alexander.Baker-1@colorado.edu>
2025-05-10 18:47:07 -0600

    Fix backend

--- ProjectSourceCode/backend/db.js
+++ ProjectSourceCode/backend/db.js
@@ -1,14 +1,14 @@
 const pgp = require('pg-promise')();
 
-// Load from environment variables (usually from .env)
-const dbConfig = {
-  host: process.env.HOST,
-  port: 5432,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD
-};
-
-const db = pgp(dbConfig);
+// Allow DATABASE_URL or fallback to local dev config
+const db = pgp(
+  process.env.DATABASE_URL || {
+    host: process.env.HOST || 'localhost',
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+  }
+);
 
 module.exports = db;
